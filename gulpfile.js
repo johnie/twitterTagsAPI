f9d3363d8215148a9164b6ba90e0d9539e0d8dfb
Sandra L <sandra@sandras-MacBook-Pro.local>
2015-05-26 15:42:52 +0200

    more improvements in responsiveness and min organization etc

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,27 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
  
 gulp.task('sass', function () {
     gulp.src('public/*.scss')
         .pipe(sass())
-        .pipe(gulp.dest('./public'));
-});
-
-gulp.task('automate', function () {
-    gulp.watch('*.scss', ['sass']);
+        .pipe(rename({suffix: '.min'})) 
+        .pipe(gulp.dest('./public/css'));
 });
  
 gulp.task('min', function() {
   return gulp.src('public/*.js')
     .pipe(uglify())
-    .pipe(gulp.dest('./public/min'));
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('./public/js'));
 });
+
+gulp.task('watch', function() {
+  gulp.watch('public/*.js', ['min']);
+  gulp.watch('public/*.scss', ['sass']);
+});
+
+// Default Tasks
+gulp.task('default', ['sass', 'watch', 'min']);
+
