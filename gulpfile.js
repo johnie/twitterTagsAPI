2eea2b1d5be3b0502444190f336695fccad41b78
Sandra <sandra@sandralundgren.com>
2015-06-04 10:37:15 +0200

    min and other org fixes

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,17 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
+var concat = require('gulp-concat');
+var minifyCSS = require('gulp-minify-css');
+
  
 gulp.task('sass', function () {
-    gulp.src('public/*.scss')
+    gulp.src('public/scss/*.scss')
         .pipe(sass())
+        .pipe(gulp.dest('./public/css'))
+        .pipe(concat('main.css'))
+        .pipe(minifyCSS())
+        .pipe(rename({suffix: '.min'}))
         .pipe(gulp.dest('./public/css'));
 });
  
